fe0e90601c7e53607f85e259188b769d712d242f
Nikita Balenok <nick.balenok@gmail.com>
2019-09-06 15:59:20 +0300

    replace domains with url

--- spec/factories.rb
+++ spec/factories.rb
@@ -17,5 +17,5 @@ end
 Factory.define(:store, :class => Spree::Store) do |f|
   f.name 'My store'
   f.code 'my_store'
-  f.domains 'www.example.com' # makes life simple, this is the default integration session domain
+  f.url 'www.example.com' # makes life simple, this is the default integration session domain
 end
